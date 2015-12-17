01c5a682975502eb80bca0e5ea06e2769897e510
Akhil Subbarao <akhilsubbarao98@gmail.com>
2015-12-16 20:28:30 -0800

    Finished truth table, and seeing if it is satisfiable, contingent, and/or valid

--- src/LogicalExpression.java
+++ src/LogicalExpression.java
@@ -2,24 +2,24 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class logicalExpression {
-	static String firstExpression = "a | b";
-	static String secondExpression = "a & b";
+	static boolean result = false;
 	static int numVariables;
 	static String[] varArray = new String[2];
+	static int returnValue = 0;
 
 	public static void main(String[] args) throws IOException {
-		// logicalExpression expression = new logicalExpression();
-		truthTable();
+		logicalExpression expression = new logicalExpression();
 	}
 
 	public logicalExpression() {
-		countVariables();
+		//countVariables();
+		returnValue = truthTable();
 		valid();
 		contingent();
 		satisfiable();
 	}
 
-	public static void countVariables() {
+	/** public static void countVariables() {
 		int ExpLength = firstExpression.length();
 		String expChar;
 		for (int i = 0; i < ExpLength; i++) {
@@ -55,10 +55,13 @@ public class logicalExpression {
 		}
 		System.out.println(Arrays.toString(varArray));
 	}
-
-	public static void truthTable() {
+*/
+	public static int truthTable() {
 		boolean a, b, c, d;
 		a = false;
+		int trueCount = 0;
+		int falseCount = 0;
+		int returnValue = 0;
 		System.out.println("a\tb\tc\td\t(a && b) && d|| !c");
 		do {
 			b = false;
@@ -67,9 +70,15 @@ public class logicalExpression {
 				do {
 					d = false;
 					System.out.println(a + "\t" + b + "\t" + c + "\t" + d + "\t" + ((a && b) && d || !c));
+					result = ((a && b) && d || !c);
+					if (result == true){
+						trueCount++;
+					}
+					else{
+						falseCount++;
+					}
 					c = !c;
-					while (d)
-						;
+					while (d);
 					d = !d;
 				} while (c);
 				// System.out.println(a + "\t" + b + "\t" + (a && b));
@@ -78,18 +87,44 @@ public class logicalExpression {
 			a = !a;
 		} while (a);
 
+		if (trueCount == 0){
+			returnValue = -1;
+		}
+		else if (falseCount == 0){
+			returnValue = 1;
+		}
+		else{
+			returnValue = 0;
+		}
+		return returnValue;
 	}
 
 	public static void valid() {
-
+		if(returnValue == 1){
+			System.out.println("The expression is valid");
+		}
+		else{
+			System.out.println("The expression is not valid"); //make the expression a variable or something so you can say the expression, 
+			// + expression + is not valid
+		}
 	}
 
 	public static void contingent() {
-
+		if(returnValue == 0){
+			System.out.println("The expression is contingent");
+		}
+		else{
+			System.out.println("The expression is not contingent");
+		}
 	}
 
 	public static void satisfiable() {
-
+		if(returnValue == -1){
+			System.out.println("The expression is not satisfiable");
+		}
+		else{
+			System.out.println("The expression is satisfiable"); 
+		}
 	}
 
 	public static void entails() {
